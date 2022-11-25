bf64c43e0bcd2f18cf4ece7aa4ad06aaa8e90438
Claudio Altamura <claudio@claudioaltamura.de>
2022-11-25 16:32:31 +0100

    chore: housekeeping

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,19 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.3"
-	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    id("com.diffplug.spotless") version "6.2.2"
-    id("com.github.ben-manes.versions") version "0.42.0"
+	id("org.springframework.boot") version "2.7.5"
+	id("io.spring.dependency-management") version "1.1.0"
+    id("com.diffplug.spotless") version "6.12.0"
+    id("com.github.ben-manes.versions") version "0.44.0"
     kotlin("jvm") version "1.6.0"
 	kotlin("plugin.spring") version "1.6.0"
 }
 
 group = "de.claudioaltamura"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+
+java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
