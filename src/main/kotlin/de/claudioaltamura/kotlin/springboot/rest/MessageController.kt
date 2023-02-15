299861516a2b3f86c872635cd63d3b0574b1901f
Claudio Altamura <claudio@claudioaltamura.de>
2023-02-15 12:52:45 +0100

    new: added api test

--- src/main/kotlin/de/claudioaltamura/kotlin/springboot/rest/MessageController.kt
+++ src/main/kotlin/de/claudioaltamura/kotlin/springboot/rest/MessageController.kt
@@ -1,11 +1,9 @@
 package de.claudioaltamura.kotlin.springboot.rest
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/")
 class MessageController(val service: MessageService) {
     @GetMapping
     fun index(): List<Message> = service.findMessages()
--- src/test/kotlin/de/claudioaltamura/kotlin/springboot/rest/KotlinSpringBootRestApplicationTests.kt
+++ src/test/kotlin/de/claudioaltamura/kotlin/springboot/rest/KotlinSpringBootRestApplicationTests.kt
@@ -1,13 +1,27 @@
 package de.claudioaltamura.kotlin.springboot.rest
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
+@SpringBootTest(classes = [KotlinSpringBootRestApplication::class],
+	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class KotlinSpringBootRestApplicationTests {
 
+	@Autowired
+	lateinit var testRestTemplate: TestRestTemplate
+
 	@Test
-	fun contextLoads() {
+	fun shouldReturnEmptyArray() {
+		val result = testRestTemplate
+			.getForEntity("/", Array<Message>::class.java)
+
+		assertNotNull(result)
+		assertEquals(HttpStatus.OK, result?.statusCode)
 	}
 
 }
