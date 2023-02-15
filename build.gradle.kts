a7f54837e819f5acd7bbb172ec348c94aaea095a
Claudio Altamura <claudio@claudioaltamura.de>
2023-02-15 12:51:55 +0100

    chore: upgrade to spring boot 3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.5"
+	id("org.springframework.boot") version "3.0.2"
 	id("io.spring.dependency-management") version "1.1.0"
-    id("com.diffplug.spotless") version "6.12.0"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
+	id("com.diffplug.spotless") version "6.12.0"
     id("com.github.ben-manes.versions") version "0.44.0"
-    kotlin("jvm") version "1.6.0"
-	kotlin("plugin.spring") version "1.6.0"
 }
 
 group = "de.claudioaltamura"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.2-SNAPSHOT"
 
 java.sourceCompatibility = JavaVersion.VERSION_17
 java.targetCompatibility = JavaVersion.VERSION_17
@@ -32,7 +32,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "11"
+		jvmTarget = "17"
 	}
 }
 
